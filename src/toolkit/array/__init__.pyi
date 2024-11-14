5dc0057ad51886279bdf814c32f2c9e9aea18e55
liblaf <30631553+liblaf@users.noreply.github.com>
2024-11-14 22:20:58 +0800

    feat: add support for scalar conversion across array libraries
    Introduce a new function to convert arrays from JAX, NumPy, and PyTorch to scalar values, enhancing interoperability and simplifying downstream processing.

--- src/toolkit/array/__init__.pyi
+++ src/toolkit/array/__init__.pyi
@@ -2,6 +2,7 @@ from . import array_like, jax, numpy, torch
 from .array_like import ArrayLike, is_array_like
 from .jax import is_jax
 from .numpy import as_dict_of_numpy, as_numpy, is_numpy
+from .python import as_scalar
 from .torch import is_torch
 
 __all__ = [
@@ -9,6 +10,7 @@ __all__ = [
     "array_like",
     "as_dict_of_numpy",
     "as_numpy",
+    "as_scalar",
     "is_array_like",
     "is_jax",
     "is_numpy",
