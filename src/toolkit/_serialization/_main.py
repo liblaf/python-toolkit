81264f4df046c5c92f9ce4d84b2bc6b39519fa83
liblaf <30631553+liblaf@users.noreply.github.com>
2024-11-17 15:37:40 +0800

    refactor: swap serialization and deserialization logic
    Improve clarity by reversing the roles of serialize and deserialize functions, ensuring correct handling of file extensions and directory creation.

--- src/toolkit/_serialization/_main.py
+++ src/toolkit/_serialization/_main.py
@@ -19,18 +19,7 @@ _WRITERS: dict[str, Callable[..., None]] = {
 }
 
 
-def serialize(fpath: tp.StrPath, *, ext: str | None = None) -> Any:
-    fpath: Path = Path(fpath)
-    if ext is None:
-        ext = fpath.suffix
-    if ext not in _READERS:
-        msg: str = f"Unsupported file extension: {ext}"
-        raise ValueError(msg)
-    reader = _READERS[ext]
-    return reader(fpath)
-
-
-def deserialize(fpath: tp.StrPath, data: Any, *, ext: str | None = None) -> None:
+def serialize(fpath: tp.StrPath, data: Any, *, ext: str | None = None) -> None:
     fpath: Path = Path(fpath)
     if ext is None:
         ext = fpath.suffix
@@ -40,3 +29,14 @@ def deserialize(fpath: tp.StrPath, data: Any, *, ext: str | None = None) -> None
     writer = _WRITERS[ext]
     fpath.parent.mkdir(parents=True, exist_ok=True)
     writer(fpath, data)
+
+
+def deserialize(fpath: tp.StrPath, *, ext: str | None = None) -> Any:
+    fpath: Path = Path(fpath)
+    if ext is None:
+        ext = fpath.suffix
+    if ext not in _READERS:
+        msg: str = f"Unsupported file extension: {ext}"
+        raise ValueError(msg)
+    reader = _READERS[ext]
+    return reader(fpath)
